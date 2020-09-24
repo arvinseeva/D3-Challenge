36647c248766bd91d9b7e378ce3ccf108d8f593a
arvinseeva <65349222+arvinseeva@users.noreply.github.com>
2020-09-24 20:59:53 +0800

    update

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,1 +1,8 @@
 // @TODO: YOUR CODE HERE!
+
+//     // SVG Wrapper dimensions are determined by the current width and height of the browser window.
+//     var svgWidth = window.innerWidth;
+//     var svgHeight = window.innerHeight;
+var svgWidth = 900;
+var svgHeight = 600;
+
