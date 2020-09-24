a454ea31810f7d9e3dbc4d3d60bc18df4cc56468
arvinseeva <65349222+arvinseeva@users.noreply.github.com>
2020-09-24 21:00:19 +0800

    Update app.js

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -6,3 +6,12 @@
 var svgWidth = 900;
 var svgHeight = 600;
 
+var margin = {
+    top: 40,
+    bottom: 90,
+    right: 40,
+    left: 100
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
