564a8eacc86a7e2a4a673ab9de867b293afd2f10
arvinseeva <65349222+arvinseeva@users.noreply.github.com>
2020-09-26 21:58:11 +0800

    Update app.js

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -15,3 +15,12 @@ var margin = {
 
 var height = svgHeight - margin.top - margin.bottom;
 var width = svgWidth - margin.left - margin.right;
+
+var svg = d3.select("#scatter")
+.append("svg")
+.attr("width", svgWidth)
+.attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
